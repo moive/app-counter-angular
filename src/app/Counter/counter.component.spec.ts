d8dd4f599658efbc959ca23824645336199653ac
Moises Velasquez <mvelasquez@pictoric.com>
2022-03-17 22:42:05 -0500

    test instant class CounterComponent

--- src/app/Counter/counter.component.spec.ts
+++ src/app/Counter/counter.component.spec.ts
@@ -1,9 +1,21 @@
+import { TestBed } from "@angular/core/testing";
 import { CounterComponent } from "./counter.component"
 
 describe("CounterComponent", () => {
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [CounterComponent]
+        }).compileComponents;
+    });
+
+    it("The Counter component must be created", () => {
+        const fixture = TestBed.createComponent(CounterComponent);
+        expect(fixture.componentInstance).toBeTruthy();
+    });
+
     it("The valor initial is 10", () => {
         const count = new CounterComponent();
 
         expect(count.counter).toBe(10)
-    })
+    });
 })
